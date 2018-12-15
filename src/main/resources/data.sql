410b36ea6f4826043c89f3b5fc64d335dc1ae3a7
vishal <vishal@fptechscience.com>
2018-12-15 21:00:07 +0530

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO rewards_summary (id, user_id, reward_id, total_rewards, total_redeemed, is_deleted, created_at, created_by, updated_at, updated_by)
+VALUES
+(1, 12, '1212', 100.00, 90.00, 0, '2018-12-15 19:02:22', NULL, '2018-12-15 19:02:22', NULL);
+
+INSERT INTO rewards_details (id, reward_id, added, redeemed, type, is_deleted, created_at, created_by, updated_at, updated_by)
+VALUES
+(1, '1212', 100.00, 0.00, 'ADD', 0, '2018-12-15 19:02:40', NULL, '2018-12-15 19:02:40', NULL),
+(2, '1212', 0.00, 90.00, 'REDEEM', 0, '2018-12-15 19:02:50', NULL, '2018-12-15 19:02:50', NULL);
+
+insert into premium_config values(1,12, 2000,0,'2018-12-15 19:02:40', NULL, '2018-12-15 19:02:40', NULL)
